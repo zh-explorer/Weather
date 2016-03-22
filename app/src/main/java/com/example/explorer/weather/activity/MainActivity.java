aedbfa18facf4507581721a60e1e54a2b9fdd0b9
zh_explorer <hsadkhk@gmail.com>
2016-03-22 12:54:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/explorer/weather/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.explorer.weather.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.explorer.weather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/explorer/weather/db/WeatherOpenHelper.java
@@ -0,0 +1,38 @@
+package com.example.explorer.weather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by explorer on 16-3-22.
+ *
+ */
+public class WeatherOpenHelper extends SQLiteOpenHelper{
+
+    /**
+     * String to  create city table
+     */
+
+    public static final String CREATE_CITY = "Create table City (" +
+            "id text primary key" +
+            "city text" +
+            "cnty text" +   //Country
+            "lat text" +    //latitude
+            "lon text" +    //longitude
+            "prov text";    //province
+
+    public WeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_CITY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
